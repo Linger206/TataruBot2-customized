10548125349cc7639a5fadba44091cc76581afd1
Linger206 <1037081683@qq.com>
2023-04-24 10:42:48 +0800

    ipv6支持

--- tatarubot2/plugins/ip.py
+++ tatarubot2/plugins/ip.py
@@ -1,5 +1,5 @@
 """
-获取公网IP, 用于ddns
+获取服务端公网IP
 仅超级用户私聊使用
 """
 
@@ -11,8 +11,6 @@ from nonebot.rule import to_me
 
 this_command = "ip"
 ip = on_fullmatch(this_command, rule=to_me(), permission=SUPERUSER, priority=5)
-# 取消代理
-proxies = {"https": ""}
 
 
 @ip.handle()
@@ -26,10 +24,15 @@ async def handle_first_receive(event: PrivateMessageEvent):
     await ip.finish(return_str)
 
 
+# 取消代理
+proxies = {"https": ""}
+
+
 async def pub_ipv4():
     try:
-        with requests.get('https://4.ident.me', proxies=proxies) as myip:
-            return myip.text
+        with requests.get('https://4.ident.me', proxies=proxies) as myip4:
+            with requests.get('https://6.ident.me', proxies=proxies) as myip6:
+                return myip4.text + "\n" + myip6.text
     except:
-        with requests.get('https://api.ipify.org', proxies=proxies) as myip:
+        with requests.get('https://api64.ipify.org', proxies=proxies) as myip:
             return myip.text
